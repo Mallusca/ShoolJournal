e7cb2d1732e72355f8bea7b253297fa9d1b43a2b
Your Name <gorosdotos@mail.ru>
2019-09-23 13:51:14 +0300

    In folder Domain Models was created different models.

---
+++ SchoolJournal/DomainModels/JournalRecord.cs
@@ -0,0 +1,18 @@
+﻿namespace SchoolJournal.DomainModels
+{
+    //TODOREAD why we should write usings inside namespace
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System;
+    using SchoolJournal.DAL.Enums;
+    using System.Collections.ObjectModel;
+
+    public class JournalRecord
+    {
+        public long Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public RecordType RecordType { get; set; }
+    }
+}
---
+++ SchoolJournal/DomainModels/JournalRow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolJournal.DomainModels
+{
+    public class JournalRow
+    {
+        public Student Student { get; set; }
+        
+        public IEnumerable<Mark> Marks { get; set; }
+    }
+}
