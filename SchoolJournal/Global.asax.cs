de0a30756f4b0d5f58805459da9e4403ce86098c
Your Name <gorosdotos@mail.ru>
2019-09-23 14:31:22 +0300

    Added Initializer.
    Added Connection Strings
    Changes packages

--- SchoolJournal/Global.asax.cs
+++ SchoolJournal/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SchoolJournal.Models.DbModel;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +13,7 @@ namespace SchoolJournal
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new JournalDbInitializator());
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
