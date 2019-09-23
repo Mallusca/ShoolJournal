8d8998f130beac140aba2c6ff5d431bd70207b86
Your Name <gorosdotos@mail.ru>
2019-09-23 13:49:05 +0300

    In Folder Data Access Layer was created three models

---
+++ SchoolJournal/DAL/Models/Mark.cs
@@ -0,0 +1,31 @@
+﻿using SchoolJournal.DAL.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SchoolJournal.DAL.Models
+{
+    public class Mark
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [ForeignKey("Student")]
+        public long StudentId { get; set; }
+
+        [ForeignKey("JournalRecord")]
+        public long JournalRecordId { get; set; }
+
+        public int Value { get; set; }
+
+        public MarkType MarkType { get; set; }
+
+        //TODOREAD navigation property
+        public virtual Student Student { get; set; }
+
+        public virtual JournalRecord JournalRecord { get; set; }
+    }
+}
